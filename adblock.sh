168983cb5ba6f69c1d71997b6fb6e63b09f70dd9
Untitled-Document-1 <jadamson60@yahoo.co.uk>
2017-09-15 11:28:23 +0100

    Significant changes

--- adblock.sh
+++ adblock.sh
@@ -1,19 +1,17 @@
 #!/bin/ash
-TMP_HOSTS=/tmp/block.hosts.unsorted
 HOSTS=/tmp/block.hosts
+THRESHOLD=65000
+TMP_HOSTS=/tmp/block.hosts.unsorted
 
-# Number of lines is 69313 as of 1 July 2016. A minimum of 65000 seems like a good indicator of success.
-threshold=65000
-today=`date +%Y-%m-%d`
-
+# If a block.hosts file already exists...
 if [ -f ${HOSTS} ]
 then
-lines_curr_file=`wc -l ${HOSTS} | awk '{print $1}'`
-date_modified=`date -r ${HOSTS} +%Y-%m-%d`
-	# Only update the file once a day, unless the file didn't meet lines threshold
-	if [ $date_modified == $today ] && [ $lines_curr_file -ge $threshold ] ;
+	EXISTING_HOSTS_LINE_COUNT=`wc -l ${HOSTS} | awk '{print $1}'`
+	YOUNG_FILE=`find ${HOSTS} -mtime -14 -print0`
+	# Do not re-create block.hosts if file younger than 14 days, OR the file didn't meet the minimum lines threshold
+	if [ -f "${YOUNG_FILE}" ] && [ ${EXISTING_HOSTS_LINE_COUNT} -ge ${THRESHOLD} ] ;
 	then
-		logger "Skipping Adblock.sh"
+		logger "Adblock.sh: skipping download"
 		exit 0
 	fi
 fi
@@ -29,33 +27,19 @@ for URL in \
     "http://hosts-file.net/ad_servers.txt" \
     "http://winhelp2002.mvps.org/hosts.txt"
 do
-    # grab a hosts file and...
-    # filter out comment lines
-    # filter out empty lines
-    # filter out localhost entries (the router is handling localhost)
-    # replace 127.0.0.1 with 0.0.0.0
-    # remove trailing comments
-    # replace tabs with spaces
-    # replace double+ spaces with single spaces
-    # remove carriage returns
-    # append the results to TMP_HOSTS
-    wget -qO- "${URL}" | grep -v -e "^#" -e "^\s*$" -e "\blocalhost\b" | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/[[:space:]]{2,}/ /" | tr -d "\r" >> ${TMP_HOSTS}
-
-# this does all of that, plus it adds an entry for ipv6 using ::1 as the address...
-# but it also has the side effect of making dnsmasq crash every 10 - 120 minutes on my wrt1900ac
-#    wget -qO- "${URL}" | grep -v -e "^#" -e "^$" -e localhost | sed -E -e "s/^127.0.0.1/0.0.0.0/" -e "s/#.*$//" -e "s/\t/ /" -e "s/  / /" -e "s/ (.+)/ \1\n::1 \1/" | tr -d "\r" >> ${TMP_HOSTS}
-
+    wget -qO- "${URL}" | awk '/^(127|0)\.0\.0\.(0|1)/{print "0.0.0.0",$2}' >> ${TMP_HOSTS}
 done
 
-lines=`wc -l ${TMP_HOSTS} | awk '{print $1}'`
 
+LINES=`wc -l ${TMP_HOSTS} | awk '{print $1}'`
 # Test if threshold met
-if [ $lines -ge $threshold ]
+if [ ${LINES} -ge ${THRESHOLD} ]
 then
     # remove duplicate hosts and save the real hosts file
-    sort ${TMP_HOSTS} | uniq > ${HOSTS}
+	sort -u ${TMP_HOSTS} > ${HOSTS}
+	logger "Adblock.sh: ${HOSTS} re-created"
 else
-    logger "Adblock.sh: TMP_HOSTS has fewer than ${threshold} lines - leaving old HOSTS alone"
+    logger "Adblock.sh: ${TMP_HOSTS} has fewer than ${THRESHOLD} lines - leaving old HOSTS alone"
 fi
 
 rm ${TMP_HOSTS} 2> /dev/null
